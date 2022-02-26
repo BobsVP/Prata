60700bdedca3edb00519b08377ab82dc419cef7a
Vladimir P <pustovalovv455@gmail.com>
2022-02-26 20:07:17 +0700

    listing Prata 15-7, Prata 15-8, Prata 15-9

--- source.cpp
+++ source.cpp
@@ -1,27 +1,34 @@
-#include</home/bobs/Prata/Header.h> 
+//#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<string>
+#include<cfloat>
+double hmean(double a, double b);
 
-int main() 
- {
-	 using std::string;
-	 using std::cin;
-	 using std::cout;
-	 QueueTP<string> cs(5);
-	 string temp;
-	while (!cs.isfull())
-	{
-		cout << "Please enter your name. You will be"
-				"served in the order of arrival.\n"
-				"name: ";
-		getline(cin, temp);
-		cs.enqueue(temp);
-	}
-	while (!cs.isempty())
-	{
-		cs.dequeue(temp);
-		cout << "Now processing " << temp << "...\n";
-	}
-	cout << "The queue is full. Processing begins!\n";
+int main()  
+{
+	 double x, y, z;
+	 std::cout << "Enter two numbers: ";
+	 while (std::cin >> x >> y)
+	 {
+		try
+		{
+			z = hmean(x, y);
+		}
+		catch(const char* s)
+		{
+			std::cout << s << '\n';
+			std::cout << "Enter next set of numbers (q to quit): ";
+			continue;
+		}
+		std::cout << "Harmonic mean of " << x << " and " << y
+		 				<< " is " << z << std::endl;
+			std::cout << "Enter next set of numbers (q to quit): ";
+	 }
+	 std::cout << "Bye!\n";
 	 return 0;
+ }
+ double hmean(double a, double b)
+ {
+	if (a == -b)
+		throw "Bad hmean() arguments: a = -b not allowed";
+	return (2.0 * a * b) / (a + b);
  }
