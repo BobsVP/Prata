0326356b8ef562871f3dd63a8b5d37bea04f0ba7
Vladimir P <pustovalovv455@gmail.com>
2022-03-16 02:34:44 +0700

    Prata 15-14, 15- 15, 15-16

--- source.cpp
+++ source.cpp
@@ -1,35 +1,74 @@
-//#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<new>
-#include<cstdlib>
-
-using namespace std;
-
-struct Big
-{
-	double staff[20000];
-};
-
+#include</home/bobs/Prata/Header.h> 
 
 int main()  
 {
-	Big* pb;
+	using std::cout;
+	using std::cin;
+	using std::endl;
+	double vals1[12] = 
+	{
+		1220, 1100, 1122, 2212, 1232, 2334,
+		2884, 2393, 3302, 2922, 3002, 3544
+	};
+	double vals2[12] = 
+	{
+		12, 11, 22, 21, 32, 34,
+		28, 29, 33, 29, 32, 35
+	};
+	Sales sales1(2011, vals1, 12);
+	LabeledSales sales2("Blogstar", 2012, vals2, 12);
+	cout << "First try block:\n";
 	try
 	{
-		cout << "Trying to get a big block of memory:\n";
-		pb = new Big[10000];
-		cout << "Got past the new request:\n";
+		int i;
+		cout << "Year = " << sales1.Year() << endl;
+		for (i = 0; i < 12; ++i)
+		{
+			cout << sales1[i] << ' ';
+			if ((i % 6) == 5)
+			cout << endl;
+		}
+		cout << "Year = " << sales2.Year() << endl;
+		cout << "Label = " << sales2.Label() << endl;
+		for (i = 0; i < 12; ++i)
+		{
+			cout << sales2[i] << ' ';
+			if ((i % 6) == 5)
+			cout << endl;
+		}
+		cout << "End of try block 1.\n";
 	}
-	catch(bad_alloc& ba)
+	catch(LabeledSales::nbad_index& bad)
 	{
-		cout << "Caught the exception!\n";
-		cout << ba.what() << endl;
-		exit(EXIT_FAILURE);
+		std::cout << bad.what();
+		std::cout << "Company: " << bad.label_val() << endl;
+		std::cout << "bad index: " << bad.bi_val() << endl;
 	}
-	cout << "Memory successfully allocated\n";
-	pb[0].staff[0] = 4;
-	cout << pb[0].staff[0] << endl;
-	delete[] pb;
+	catch(Sales::bad_index& bad)
+	{
+		std::cout << bad.what();
+		std::cout << "bad index: " << bad.bi_val() << endl;
+	}
+	cout << "\nNext try block:\n";
+	try
+	{
+		sales2[2] = 37.5;
+		sales1[20] = 23345;
+		cout << "End of try block 2.\n";
+	}
+	catch(LabeledSales::nbad_index& bad)
+	{
+		std::cout << bad.what();
+		std::cout << "Company: " << bad.label_val() << endl;
+		std::cout << "bad index: " << bad.bi_val() << endl;
+	}
+	catch(Sales::bad_index& bad)
+	{
+		std::cout << bad.what();
+		std::cout << "bad index: " << bad.bi_val() << endl;
+	}
+	cout << "Done.\n";
 	return 0;
  }
 
