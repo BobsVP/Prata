2f55db7a8c308bf36d801e2571a45773c76bd76c
Vladimir P <pustovalovv455@gmail.com>
2022-03-20 11:18:56 +0700

    Prata 15-19

--- source.cpp
+++ source.cpp
@@ -1,70 +1,25 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<cstdlib>
-#include<ctime>
 
-using std::cout;
+using std::cout; 
+using std::endl; 
 
-class Grand
-{
-private:
-	int hold;
-public:
-	Grand(int h = 0) : hold(h) {}
-	virtual void Speak() const {cout << "I am a Grand class!\n";}
-	virtual int Value() const {return hold;}
-};
-
-class Superb : public Grand
-{
-public:
-	Superb(int h = 0):Grand(h) {}
-	void Speak() const {cout << "I am a Superb class!\n";}
-	virtual void Say() const {cout << "I hold the Superb value of " << Value() << "!\n";}
-};
-
-class Magnificent : public Superb
-{
-private:
-	char ch;
-public:
-	Magnificent(int h = 0, char c = 'A'):Superb(h), ch(c) {}
-	void Speak() const {cout << "I am a Magnificent class!\n";}
-	void Say() const {cout << "I hold the character " << ch << " and the integer " << Value() << "!\n";}
-};
-
-Grand* GetOne();
+void change(const int* pt, int n);
 
 int main()  
 {
-	std::srand(std::time(0));
-	Grand* pg;
-	Superb* ps;
-	for (int i = 0; i < 5; ++i)
-	{
-		pg = GetOne();
-		cout << "Now processing tipe " << typeid(*pg).name() << "!\n";
-		pg->Speak();
-		if (ps = dynamic_cast<Superb*>(pg))
-			ps->Say();
-		if (typeid(Magnificent) == typeid(*pg))
-			cout << "Yes, you're really magnificent.\n";
-	}
-	
+	int pop1 = 38383;
+	const int pop2 = 2000;
+	cout << "pop1, pop2: " << pop1 << ", " << pop2 << endl;
+	change(&pop1, -103);
+	change(&pop2, -103);
+	cout << "pop1, pop2: " << pop1 << ", " << pop2 << endl;
 	return 0;
- }
+}
 
-Grand* GetOne()
+void change(const int* pt, int n)
 {
-	Grand* p;
-	switch (std::rand() % 3)
-	{
-	case 0: p = new Grand(std::rand() % 100);
-		break;
-	case 1: p = new Superb(std::rand() % 100);
-		break;
-	case 2: p = new Magnificent(std::rand() % 100, 'A' + std::rand()% 26);
-		break;
-	}
-	return p;
+	int* pc;
+	pc = const_cast<int*>(pt);
+	*pc += n;
 }
