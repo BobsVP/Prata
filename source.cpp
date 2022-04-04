42a4753d86e9ee42295141b3cc19a24f37cbd1db
Vladimir P <pustovalovv455@gmail.com>
2022-04-05 01:46:52 +0700

    Prata 16-4

--- source.cpp
+++ source.cpp
@@ -1,86 +1,24 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<ctime>
 #include<string>
-#include<cstdlib>
-#include<cctype>
-
-using std::string;
-const int NUM = 26;
-const string worldlist[NUM] = {"apiary", "beetle", "cereal", 
-	"danger", "ensign", "florid", "garage", "health", "insult",
-	"jackal", "keeper", "loaner", "manage", "nonce", "onset",
-	"plaid", "quilt", "remote", "stolid", "train", "useful",
-	"valid", "whence", "xenon", "yearn", "zippy"};
 
 int main()  
 {
-	using std::cout;
-	using std::cin;
-	using std::tolower;
-	using std::endl;
-	std::srand(std::time(0));
-	char play;
-	cout << "Will you play a world game? <y, n>: ";
-	cin >> play;
-	play = tolower(play);
-	while (play == 'y')
-	{
-		string target = worldlist[std::rand() % NUM];
-		int lenght = target.length();
-		string attempt(lenght, '-');
-		string badchars;
-		int guesses = 6;
-		cout << "Guess my secret word. It has " << lenght
-			<< " letters, and you guess\n"
-			<< "one letter at a time, You get " << guesses
-			<< " wrong guesses.\n";
-		cout << "Your word: " << attempt << endl;
-		while (guesses > 0 && attempt != target)
-		{
-			char letter;
-			cout << "Guess a letter: ";
-			cin >> letter;
-			if(badchars.find(letter) != string::npos || attempt.find(letter) != string::npos)
-			{
-				cout << "You already guessed that. Try again.\n";
-				continue;
-			}
-			int loc = target.find(letter);
-			if (loc == string::npos)
-			{
-				cout << "Oh, bad guess!\n";
-				--guesses;
-				badchars += letter;
-			}
-			else
-			{
-				cout << "God guess!\n";
-				attempt[loc] = letter;
-				loc = target.find(letter, loc + 1);
-				while (loc != string::npos)
-				{
-					attempt[loc] = letter;
-					loc = target.find(letter, loc + 1);
-				}
-			}
-			cout << "You word: " << attempt << endl;
-			if (attempt != target)
-			{
-				if (badchars.length() > 0)
-				cout << "Bad choices: " << badchars << endl;
-				cout << guesses << " bad guesses left\n";
-			}
-		}
-		if (guesses > 0)
-			cout << "That's right!\n";
-		else
-			cout << "Sorry, the word is " << target << ".\n";
-		cout << "Will you play another? <y,n> ";
-		cin >> play;
-		play = tolower(play);
-	}
-	cout << "Bye. \n";
+	using namespace std;
+	string empty;
+	string small = "bit";
+	string larger = "Elephants are а girl's best friend";
+	cout << "Sizes:\n";
+	cout << "\tempty: " << empty.size() << endl;
+	cout << "\tsmall: " << small.size() << endl;
+	cout << "\tlarger: " << larger.size() << endl;
+	cout << "Capasities:\n";
+	cout << "\tempty: " << empty.capacity() << endl;
+	cout << "\tsmall: " << small.capacity() << endl;
+	cout << "\tlarger: " << larger.capacity() << endl;
+	empty.reserve(50);
+	cout << "Capasity after empty.reserve(50): "
+		<< empty.capacity() << endl;
 	return 0;
 }
 
