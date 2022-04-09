1014c3e20ed244a51d90546756dfaf299a12ab3d
Vladimir P <pustovalovv455@gmail.com>
2022-04-09 20:45:26 +0700

    Prata 16-8

--- source.cpp
+++ source.cpp
@@ -3,30 +3,70 @@
 #include<string>
 #include<vector>
 
-const int NUM = 5;
+struct Review
+{
+	std::string title;
+	int rating;
+};
+
+bool FillReview(Review& rr);
+void ShowReview(const Review& rr);
 
 int main()  
 {
 	using std::vector;
-	using std::string;
 	using std::cout;
-	using std::cin;
-	using std::endl;
-	vector<int> Rating(NUM);
-	vector<string> titles(NUM);
-	cout << "You will do exactly as told. You will enter\n" << NUM << " book titles and your ratings (0-10).\n";
-	int i;
-	for (i = 0; i < NUM; i++)
+	vector<Review> books;
+	Review temp;
+	while (FillReview(temp))
+		books.push_back(temp);
+	int num = books.size();
+	if (num > 0)
 	{
-		cout << "Enter title #" << i + 1 << ": ";
-		getline(cin, titles[i], '\n');
-		cout << "Enter your rating(0-10): ";
-		cin >> Rating[i];
-		cin.get();
+		cout << "Thank you. You entering the following:\n" << "Rating\tBook\n";
+		for (int i = 0; i < num; i++)
+			ShowReview(books[i]);
+		cout << "Reprising:\n" << "Rating\tBook\n";
+		vector<Review>::iterator pr;
+		for (pr = books.begin(); pr != books.end(); pr++)
+			ShowReview(*pr);
+		vector<Review> oldlist(books);
+		if (num > 3)
+		{
+			books.erase(books.begin() + 1, books.begin() + 3);
+			cout << "After erasure:\n";
+			for (pr = books.begin(); pr != books.end(); pr++)
+				ShowReview(*pr);
+			books.insert(books.begin(), oldlist.begin() + 1, oldlist.begin() + 2);
+			cout << "After insertion:\n";
+			for (pr = books.begin(); pr != books.end(); pr++)
+				ShowReview(*pr);
+		}
+		books.swap(oldlist);
+		cout << "Swapping oldlist with books:\n";
+		for (pr = books.begin(); pr != books.end(); pr++)
+			ShowReview(*pr);
 	}
-	cout << "Thank you. You entring the following:\n" << "Rating\tBook\n";
-	for (i = 0; i < NUM; i++)
-		cout << Rating[i] << "\t" << titles[i] << endl;
+	else
+		cout << "Nothing entered, nothing gained.\n";
 	return 0;
 }
 
+bool FillReview(Review& rr)
+{
+	std::cout << "Enter book title(quit to quit): ";
+	std::getline(std::cin, rr.title);
+	if (rr.title == "quit")
+		return false;
+	std::cout << "Enter book rating: ";
+	std::cin >> rr.rating;
+	if(!std::cin)
+		return false;
+	while(std::cin.get() != '\n')
+		continue;
+	return true;
+}
+void ShowReview(const Review& rr)
+{
+	std::cout << rr.rating << "\t" << rr.title << std::endl;
+}
