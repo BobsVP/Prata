16c35f7c6252f65420766fe6f48f6f360fbb7a52
Vladimir P <pustovalovv455@gmail.com>
2022-04-17 03:31:53 +0700

    Prata 16-10

--- source.cpp
+++ source.cpp
@@ -1,81 +1,26 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<string>
+#include<iterator>
 #include<vector>
-#include<algorithm>
-
-struct Review
-{
-	std::string title;
-	int rating;
-};
-
-bool operator<(const Review& r1, const Review& r2);
-bool worseThan(const Review& r1, const Review& r2);
-bool FillReview(Review& rr);
-void ShowReview(const Review& rr);
 
 int main()  
 {
 	using namespace std;
-	vector<Review> books;
-	Review temp;
-	while (FillReview(temp))
-		books.push_back(temp);
-	if (books.size() > 0)
-	{
-		cout << "Thank you. You entering the following " << books.size() << " ratings:\n" << "Rating\tBook\n";
-		for_each(books.begin(), books.end(), ShowReview);
-		sort(books.begin(), books.end());
-		cout << "Sorted by title:\n" << "Rating\tBook\n";
-		for_each(books.begin(), books.end(), ShowReview);
-		sort(books.begin(), books.end(), worseThan);
-		cout << "Sorted by rating:\n" << "Rating\tBook\n";
-		for_each(books.begin(), books.end(), ShowReview);
-		vector<Review>::iterator pr;
-		random_shuffle(books.begin(), books.end());
-		cout << "After snuffling:\n" << "Rating\tBook\n";
-		for_each(books.begin(), books.end(), ShowReview);
-	}
-	else
-		cout << "No entries. ";
-	cout << "Bye!\n";
+	int casts[10] = {6, 7, 2, 9, 4, 11, 8, 7, 10, 5};
+	vector<int> dice(10);
+	copy(begin(casts), end(casts), dice.begin());
+	cout << "Let the dice be cast!\n";
+	ostream_iterator<int, char> out_iter(cout, " ");
+	copy(dice.begin(), dice.end(), out_iter);
+	cout << endl;
+	cout << "Implicit use of reverse iterator.\n";
+	copy(dice.rbegin(), dice.rend(), out_iter);
+	cout << endl;
+	cout << "Explicit use of reverse iterator.\n";
+	vector<int>::reverse_iterator ri;
+	for (ri = dice.rbegin(); ri != dice.rend(); ++ri)
+		cout << *ri << ' ';
+	cout << endl;
 	return 0;
 }
 
-bool operator<(const Review& r1, const Review& r2)
-{
-	if(r1.title < r2.title)
-		return true;
-	else if (r1.title == r2.title && r1.rating < r2.rating)
-		return true;
-	else	
-		return false;
-}
-bool worseThan(const Review& r1, const Review& r2)
-{
-	if (r1.rating < r2.rating)
-		return true;
-	else	
-		return false;
-}
-
-
-bool FillReview(Review& rr)
-{
-	std::cout << "Enter book title(quit to quit): ";
-	std::getline(std::cin, rr.title);
-	if (rr.title == "quit")
-		return false;
-	std::cout << "Enter book rating: ";
-	std::cin >> rr.rating;
-	if(!std::cin)
-		return false;
-	while(std::cin.get() != '\n')
-		continue;
-	return true;
-}
-void ShowReview(const Review& rr)
-{
-	std::cout << rr.rating << "\t" << rr.title << std::endl;
-}
