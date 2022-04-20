1136e2e23295fc78851ef2ed7e908eb93d4703d8
Vladimir P <pustovalovv455@gmail.com>
2022-04-21 01:45:43 +0700

    Prata 16-12

--- source.cpp
+++ source.cpp
@@ -1,27 +1,46 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<string>
+#include<list>
 #include<iterator>
-#include<vector>
 #include<algorithm>
 
-void output(const std::string& s) { std::cout << s << " "; }
+void outint(int n) { std::cout << n << " "; }
 
 int main()  
 {
 	using namespace std;
-	string s1[4] = {"fine", "fish", "fashion", "fate"};
-	string s2[2] = {"busy", "bats"};
-	string s3[2] = {"silly", "singers"};
-	vector<string> words(4);
-	copy(s1, s1 + 4, words.begin());
-	for_each(words.begin(), words.end(), output);
-	cout << endl;
-	copy(s2, s2 + 2, back_insert_iterator<vector<string>>(words));
-	for_each(words.begin(), words.end(), output);
-	cout << endl;
-	copy(s3, s3 + 2, insert_iterator<vector<string>>(words, words.begin()));
-	for_each(words.begin(), words.end(), output);
+	list<int> one(5, 2);
+	int stuff[5] = {1, 2, 4, 8, 6};
+	list<int> two;
+	two.insert(two.begin(), stuff, stuff + 5);
+	int more[6] = {6, 4, 2, 4, 6, 5};
+	list<int> three(two);
+	three.insert(three.end(), more, more + 6);
+	cout << "List one: ";
+	for_each(one.begin(), one.end(), outint);
+	cout << endl << "List two: ";
+	for_each(two.begin(), two.end(), outint);
+	cout << endl << "List three: ";
+	for_each(three.begin(), three.end(), outint);
+	three.remove(2);
+	cout << endl << "List three minus 2s: ";
+	for_each(three.begin(), three.end(), outint);
+	three.splice(three.begin(), one);
+	cout << endl << "List three after splice: ";
+	for_each(three.begin(), three.end(), outint);
+	cout << endl << "List one: ";
+	for_each(one.begin(), one.end(), outint);
+	three.unique();
+	cout << endl << "List three after unique: ";
+	for_each(three.begin(), three.end(), outint);
+	three.sort();
+	three.unique();
+	cout << endl << "List three after sort&unique: ";
+	for_each(three.begin(), three.end(), outint);
+	two.sort();
+	three.merge(two);
+	cout << endl << "Sorted two merged into three: ";
+	for_each(three.begin(), three.end(), outint);
 	cout << endl;
 	return 0;
 }
