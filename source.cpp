0548780b248a3eddc21bb4513c5cc1bfb8c340d1
Vladimir P <pustovalovv455@gmail.com>
2022-04-24 10:49:59 +0700

    Prata 16-14

--- source.cpp
+++ source.cpp
@@ -1,47 +1,37 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
 #include<string>
-#include<set>
-#include<iterator>
+#include<map>
 #include<algorithm>
+
+using KeyType = int;
+using Pair = std::pair<const KeyType, std::string>;
+using MapCode = std::multimap<KeyType, std::string>;
 
 int main()  
 {
 	//using namespace std;
-	const int N = 6;
-	std::string s1[N] = {"buffoon", "thinkers", "for", "heavy", "can", "for"};
-	std::string s2[N] = {"metal", "any", "food", "elegant", "deliver", "for"};
-	std::set<std::string> A(s1, s1 + N);
-	std::set<std::string> B(s2, s2 + N);
-	std::ostream_iterator<std::string, char> out(std::cout, " ");
-	std::cout << "Set A: ";
-	copy(A.begin(), A.end(), out);
-	std::cout << std::endl;
-	std::cout << "Set B: ";
-	copy(B.begin(), B.end(), out);
-	std::cout << std::endl;
-	std::cout << "Union of A and B:\n";
-	std::set_union(A.begin(), A.end(), B.begin(), B.end(), out);
-	std::cout << std::endl;
-	std::cout << "Intersection of A and B:\n";
-	std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), out);
-	std::cout << std::endl;
-	std::cout << "Difference of A and B:\n";
-	std::set_difference(A.begin(), A.end(), B.begin(), B.end(), out);
-	std::cout << std::endl;
-	std::set<std::string> C;
-	std::cout << "Zet C:\n";
-	std::set_union(A.begin(), A.end(), B.begin(), B.end(), std::insert_iterator<std::set<std::string>>(C, C.begin()));
-	copy(C.begin(), C.end(), out);
-	std::cout << std::endl;
-	std::string s3("grungy");
-	C.insert(s3);
-	std::cout << "Set C after insertion:\n";
-	copy(C.begin(), C.end(), out);
-	std::cout << std::endl;
-	std::cout << "Showing a range:\n";
-	std::copy(C.lower_bound("ghost"), C.upper_bound("spook"), out);
-	std::cout << std::endl;
+	MapCode codes;
+	codes.insert(Pair(415, "San Francisco"));
+	codes.insert(Pair(510, "Oakland"));
+	codes.insert(Pair(718, "Brooklyn"));
+	codes.insert(Pair(718, "Staten Island"));
+	codes.insert(Pair(415, "San Rafael"));
+	codes.insert(Pair(510, "Berkeley"));
+	std::cout << "Number of cities with area code 415: "
+		<< codes.count(415) << std::endl;
+	std::cout << "Number of cities with area code 718: "
+		<< codes.count(718) << std::endl;
+	std::cout << "Number of cities with area code 510: "
+		<< codes.count(510) << std::endl;
+	std::cout << "Area Code City:\n";
+	MapCode::iterator it;
+	for (it = codes.begin(); it != codes.end(); ++it)
+		std::cout << " " << (*it).first << " " << (*it).second << std::endl;
+	std::pair<MapCode::iterator, MapCode::iterator> range = codes.equal_range(718);
+	std::cout << "Cities with area code 718:\n";
+	for (it = range.first; it != range.second; ++it)
+		std::cout << (*it).second << std::endl;
 	return 0;
 }
 
