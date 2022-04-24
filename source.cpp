046c296f1416323c647c59240a6fc6a52a302f2a
Vladimir P <pustovalovv455@gmail.com>
2022-04-24 12:57:46 +0700

    Prata 16-15

--- source.cpp
+++ source.cpp
@@ -1,37 +1,43 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<string>
-#include<map>
+#include<list>
+#include<iterator>
 #include<algorithm>
 
-using KeyType = int;
-using Pair = std::pair<const KeyType, std::string>;
-using MapCode = std::multimap<KeyType, std::string>;
+template<typename T>
+class TooBig
+{
+private:
+	T cutoff;
+public:
+	TooBig(const T& t):cutoff(t) {};
+	bool operator()(const T& v) { return v > cutoff; }
+};
+
+void outint(int n) {std::cout << n << " ";}
 
 int main()  
 {
-	//using namespace std;
-	MapCode codes;
-	codes.insert(Pair(415, "San Francisco"));
-	codes.insert(Pair(510, "Oakland"));
-	codes.insert(Pair(718, "Brooklyn"));
-	codes.insert(Pair(718, "Staten Island"));
-	codes.insert(Pair(415, "San Rafael"));
-	codes.insert(Pair(510, "Berkeley"));
-	std::cout << "Number of cities with area code 415: "
-		<< codes.count(415) << std::endl;
-	std::cout << "Number of cities with area code 718: "
-		<< codes.count(718) << std::endl;
-	std::cout << "Number of cities with area code 510: "
-		<< codes.count(510) << std::endl;
-	std::cout << "Area Code City:\n";
-	MapCode::iterator it;
-	for (it = codes.begin(); it != codes.end(); ++it)
-		std::cout << " " << (*it).first << " " << (*it).second << std::endl;
-	std::pair<MapCode::iterator, MapCode::iterator> range = codes.equal_range(718);
-	std::cout << "Cities with area code 718:\n";
-	for (it = range.first; it != range.second; ++it)
-		std::cout << (*it).second << std::endl;
+	using std::list;
+	using std::cout;
+	using std::endl;
+	TooBig<int> f100(100);
+	int vals[10] = {50, 100, 90, 180, 60, 210, 415, 88, 188, 201};
+	list<int> yadayada(vals, vals + 10);
+	list<int> etcetera(vals, vals + 10);
+	cout << "Original list:\n";
+	for_each(yadayada.begin(), yadayada.end(), outint);
+	cout << endl;
+	for_each(etcetera.begin(), etcetera.end(), outint);
+	cout << endl;
+	yadayada.remove_if(f100);
+	etcetera.remove_if(TooBig<int>(200));
+	yadayada.sort();
+	etcetera.sort();
+	for_each(yadayada.begin(), yadayada.end(), outint);
+	cout << endl;
+	for_each(etcetera.begin(), etcetera.end(), outint);
+	cout << endl;
 	return 0;
 }
 
