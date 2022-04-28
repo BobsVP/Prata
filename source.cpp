ef193c7c4b8c8ef0859114790d5324b62d1c0e86
Vladimir P <pustovalovv455@gmail.com>
2022-04-29 02:29:06 +0700

    Prata 16-18

--- source.cpp
+++ source.cpp
@@ -1,22 +1,40 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<string>
+#include<list>
 #include<algorithm>
+
+void Show(int);
+const int LIM = 10;
 
 int main()  
 {
 	//using namespace std;
-	std::string letters;
-	std::cout << "Enter the letter grouping (quit to quit): ";
-	while (std::cin >> letters && letters != "quit")
-	{
-		std::cout << "Permutations of " << letters << std::endl;
-		sort(letters.begin(), letters.end());
-		std::cout << letters << std::endl;
-		while (std::next_permutation(letters.begin(), letters.end()))
-			std::cout << letters << std::endl;
-		std::cout << "Enter next sequence (quit to quit): ";
-	}
-	std::cout << "Done.\n";
+	int ar[LIM] = {4, 5, 4, 2, 2, 3, 4, 8, 1, 4};
+	std::list<int> la(ar, ar + LIM);
+	std::list<int> lb(la);
+	std::cout << "Original list content:\n\t";
+	std::for_each(la.begin(), la.end(), Show);
+	std::cout << std::endl;
+	la.remove(4);
+	std::cout << "After using the remove() method:\n";
+	std::cout << "la:\t";
+	std::for_each(la.begin(), la.end(), Show);
+	std::cout << std::endl;
+	std::list<int>::iterator last;
+	last = remove(lb.begin(), lb.end(), 4);
+	std::cout << "After using the remove() function:\n";
+	std::cout << "lb:\t";
+	std::for_each(lb.begin(), lb.end(), Show);
+	std::cout << std::endl;
+	lb.erase(last, lb.end());
+	std::cout << "After using the erase() method:\n";
+	std::cout << "lb:\t";
+	std::for_each(lb.begin(), lb.end(), Show);
+	std::cout << std::endl;
 	return 0;
+}
+
+void Show(int v)
+{
+	std::cout << v << " ";
 }
