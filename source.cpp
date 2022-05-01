7ee345192d94a70597443db1e44363ea3c694fd7
Vladimir P <pustovalovv455@gmail.com>
2022-05-01 15:06:10 +0700

    Prata 16-21

--- source.cpp
+++ source.cpp
@@ -1,37 +1,51 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
 #include<valarray>
-#include<vector>
-#include<algorithm>
+#include<cstdlib>
+
+const int SIZE = 12;
+using vint = std::valarray<int>;
+void show(const vint& v, int cols);
 
 int main()  
 {
-	//using namespace std;
-	std::vector<double> data;
-	double temp;
-	std::cout << "Enter numbers (0<= to quit):\n";
-	while (std::cin >> temp && temp > 0)
-		data.push_back(temp);
-	std::sort(data.begin(), data.end());
-	int size = data.size();
-	std::valarray<double> numbers(size);
+	using std::slice;
+	using std::cout;
+	vint valint(SIZE);
 	int i;
-	for ( i = 0; i < size; i++)
-		numbers[i] = data[i];
-	std::valarray<double> sq_rts(size);
-	sq_rts = sqrt(numbers);
-	std::valarray<double> result(size);
-	result = numbers + 2.0 * sq_rts;
-	std::cout.setf(std::ios_base::fixed);
-	std::cout.precision(4);
-	for ( i = 0; i < size; i++)
-	{
-		std::cout.width(8);
-		std::cout << numbers[i] << ": ";
-		std::cout.width(8);
-		std::cout << result[i] << std::endl;
-	}
-	std::cout << "done.\n";
+	for ( i = 0; i < SIZE; i++)
+		valint[i] = std::rand() % 10;
+	cout << "Original array:\n";
+	show(valint, 3);
+	vint vcol(valint[slice(1, 4, 3)]);
+	cout << "Second column:\n";
+	show(vcol, 1);
+	vint vrow(valint[slice(3, 3, 1)]);
+	cout << "Second row:\n";
+	show(vrow, 3);
+	valint[slice(2, 4, 3)] = 10;
+	cout << "Set last column to 10:\n";
+	show(valint, 3);
+	cout << "Set first column to sum of next two:\n";
+	valint[slice(0, 4, 3)] = vint(valint[slice(1, 4, 3)]) + vint(valint[slice(2, 4, 3)]);
+	show(valint, 3);
 	return 0;
 }
 
+void show(const vint& v, int cols)
+{
+	using std::cout;
+	using std::endl;
+	int lim = v.size();
+	for (int i = 0; i < lim; ++i)
+	{
+		cout.width(3);
+		cout << v[i];
+		if (i % cols == cols - 1)
+			cout << endl;
+		else
+			cout << " ";
+	}
+	if (lim % cols != 0)
+		cout << endl;
+}
