3ea94300bf325f9ec844a48082072b9f1a0d6dcc
Vladimir P <pustovalovv455@gmail.com>
2022-05-01 13:32:31 +0700

    Prata 16-20

--- source.cpp
+++ source.cpp
@@ -1,49 +1,37 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<string>
+#include<valarray>
 #include<vector>
-#include<set>
-#include<map>
-#include<iterator>
 #include<algorithm>
-#include<cctype>
-
-char toLower(char ch) {return tolower(ch);}
-std::string& ToLower(std::string& st);
-void display(const std::string& s);
 
 int main()  
 {
 	//using namespace std;
-	std::vector<std::string> words;
-	std::cout << "Enter words (enter quit to quit):\n";
-	std::string input;
-	while (std::cin >> input && input != "quit")
-		words.push_back(input);
-	std::cout << "You entered the following words:\n";
-	std::for_each(words.begin(), words.end(), display);
-	std::cout << "\n";
-	std::set<std::string> wordset;
-	std::transform(words.begin(), words.end(), std::insert_iterator<std::set<std::string>>(wordset, wordset.begin()), ToLower);
-	std::cout << "\nAlphabetic list of words:\n";
-	std::for_each(wordset.begin(), wordset.end(), display);
-	std::cout << std::endl;
-	std::map<std::string, int> wordsmap;
-	std::set<std::string>::iterator si;
-	for (si = wordset.begin(); si != wordset.end(); ++si)
-		wordsmap[*si] = std::count(words.begin(), words.end(), *si);
-	std::cout << "\nWords frequency:\n";
-	for (si = wordset.begin(); si != wordset.end(); ++si)
-		std::cout << *si << ": " << wordsmap[*si] << std::endl;
+	std::vector<double> data;
+	double temp;
+	std::cout << "Enter numbers (0<= to quit):\n";
+	while (std::cin >> temp && temp > 0)
+		data.push_back(temp);
+	std::sort(data.begin(), data.end());
+	int size = data.size();
+	std::valarray<double> numbers(size);
+	int i;
+	for ( i = 0; i < size; i++)
+		numbers[i] = data[i];
+	std::valarray<double> sq_rts(size);
+	sq_rts = sqrt(numbers);
+	std::valarray<double> result(size);
+	result = numbers + 2.0 * sq_rts;
+	std::cout.setf(std::ios_base::fixed);
+	std::cout.precision(4);
+	for ( i = 0; i < size; i++)
+	{
+		std::cout.width(8);
+		std::cout << numbers[i] << ": ";
+		std::cout.width(8);
+		std::cout << result[i] << std::endl;
+	}
+	std::cout << "done.\n";
 	return 0;
 }
 
-std::string& ToLower(std::string& st)
-{
-	std::transform(st.begin(), st.end(), st.begin(), toLower);
-	return st;
-}
-void display(const std::string& s)
-{
-	std::cout << s << " ";
-}
