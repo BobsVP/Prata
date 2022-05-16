39dda24a0fcfdd3c2a7c6f127a6f55e2aac08ef0
Vladimir P <pustovalovv455@gmail.com>
2022-05-17 01:38:11 +0700

    Prata 17-1

--- source.cpp
+++ source.cpp
@@ -1,51 +1,26 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
-#include<valarray>
-#include<cstdlib>
-
-const int SIZE = 12;
-using vint = std::valarray<int>;
-void show(const vint& v, int cols);
+#include<cstring>
 
 int main()  
 {
-	using std::slice;
-	using std::cout;
-	vint valint(SIZE);
-	int i;
-	for ( i = 0; i < SIZE; i++)
-		valint[i] = std::rand() % 10;
-	cout << "Original array:\n";
-	show(valint, 3);
-	vint vcol(valint[slice(1, 4, 3)]);
-	cout << "Second column:\n";
-	show(vcol, 1);
-	vint vrow(valint[slice(3, 3, 1)]);
-	cout << "Second row:\n";
-	show(vrow, 3);
-	valint[slice(2, 4, 3)] = 10;
-	cout << "Set last column to 10:\n";
-	show(valint, 3);
-	cout << "Set first column to sum of next two:\n";
-	valint[slice(0, 4, 3)] = vint(valint[slice(1, 4, 3)]) + vint(valint[slice(2, 4, 3)]);
-	show(valint, 3);
-	return 0;
-}
-
-void show(const vint& v, int cols)
-{
 	using std::cout;
 	using std::endl;
-	int lim = v.size();
-	for (int i = 0; i < lim; ++i)
+	const char *state1 = "Florida";
+	const char *state2 = "Kansas";
+	const char *state3 = "Euphoria";
+	int len = std::strlen(state2);
+	cout << "Increasing loop index:\n";
+	int i;
+	for ( i = 1; i <= len; i++)
 	{
-		cout.width(3);
-		cout << v[i];
-		if (i % cols == cols - 1)
-			cout << endl;
-		else
-			cout << " ";
-	}
-	if (lim % cols != 0)
+		cout.write(state2, i);
 		cout << endl;
+	}
+	cout << "Decreasing loop index:\n";
+	for (i = len; i > 0; i--)
+		cout.write(state2, i) << endl;
+	cout << "Exceeding string length:\n";
+		cout.write(state2, len + 5) << endl;
+	return 0;
 }
