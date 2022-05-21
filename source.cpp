2c1dca1e2f75d4110deeba2a0ad68b43d8774a7a
Vladimir P <pustovalovv455@gmail.com>
2022-05-22 02:03:49 +0700

    Prata 17-8

--- source.cpp
+++ source.cpp
@@ -5,13 +5,19 @@ int main()
 {
 	using std::cout;
 	using std::ios_base;
-	float price1 = 20.40;
-	float price2 = 1.9 + 8.0 / 9.0;
-	cout.setf(ios_base::showpoint);
-	cout << "\"Furry Friends\" is $" << price1 << "!\n";
-	cout << "\"Fiery Friends\" is $" << price2 << "!\n";
-	cout.precision(2);
-	cout << "\"Furry Friends\" is $" << price1 << "!\n";
-	cout << "\"Fiery Friends\" is $" << price2 << "!\n";
+	using std::endl;
+	int temperature = 63;
+	cout << "Today's water temperature: ";
+	cout.setf(ios_base::showpos);
+	cout << temperature << endl;
+	cout << "For our programming friends, that's\n";
+	cout << std::hex << temperature << endl;
+	cout.setf(ios_base::uppercase);
+	cout.setf(ios_base::showbase);
+	cout << "or\n";
+	cout << temperature << endl;
+	cout << "How " << true << "! oops - How ";
+	cout.setf(ios_base::boolalpha);
+	cout << true << "!\n";
 	return 0;
 }
