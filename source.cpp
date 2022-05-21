b2089fcbc792c70d32bdbc2259391079aecf9758
Vladimir P <pustovalovv455@gmail.com>
2022-05-22 01:45:57 +0700

    Prata 17-7

--- source.cpp
+++ source.cpp
@@ -4,8 +4,10 @@
 int main()  
 {
 	using std::cout;
+	using std::ios_base;
 	float price1 = 20.40;
 	float price2 = 1.9 + 8.0 / 9.0;
+	cout.setf(ios_base::showpoint);
 	cout << "\"Furry Friends\" is $" << price1 << "!\n";
 	cout << "\"Fiery Friends\" is $" << price2 << "!\n";
 	cout.precision(2);
