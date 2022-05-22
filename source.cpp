d5793bdbeb29c5dce658876259cdbeb17dcf84c3
Vladimir P <pustovalovv455@gmail.com>
2022-05-22 12:06:50 +0700

    Prata 17-12

--- source.cpp
+++ source.cpp
@@ -1,15 +1,25 @@
 //#include</home/bobs/Prata/Header.h> 
 #include<iostream>
+#include<exception>
 
 int main()  
 {
 	using namespace std;
+	cin.exceptions(ios_base::failbit);
 	cout << "Enter numbers: ";
 	int sum = 0;
 	int input;
-	while (cin >> input)
+	try
 	{
-		sum += input;
+		while(cin >> input)
+		{
+			sum += input;
+		}
+	}
+	catch(ios_base::failure& bf)
+	{
+		std::cerr << bf.what() << '\n';
+		cout << "O! the horror!\n";
 	}
 	cout << "Last value entered = " << input << endl;
 	cout << "Sum = " << sum << endl;
