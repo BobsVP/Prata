2c3a5f265c0f459401f2581ec66c7ba2a2a44812
Vladimir P <pustovalovv455@gmail.com>
2022-05-30 01:01:12 +0700

    Prata 17-20

--- source.cpp
+++ source.cpp
@@ -4,10 +4,11 @@
 #include<iomanip>
 #include<cstdlib>
 
+const int LIM = 20;
 inline void eatline() { while (std::cin.get() != '\n') continue; }
 struct planet
 {
-	char name[20];
+	char name[LIM];
 	double population;
 	double g;
 };
@@ -17,54 +18,80 @@ int main()
 {
 	using namespace std;
 	planet pl;
-	cout << fixed << right;
-	ifstream fin;
-	fin.open(file, ios_base::in|ios_base::binary);
-	if (fin.is_open())
+	cout << fixed;
+	fstream finout;
+	finout.open(file, ios_base::in|ios_base::binary|ios_base::out);
+	int ct = 0;
+	if (finout.is_open())
 	{
+		finout.seekg(0);
 		cout << "Here are the current contents of the " << file << " file:\n";
-		while (fin.read((char*) &pl, sizeof pl))
+		while (finout.read((char*) &pl, sizeof pl))
 		{
-			cout << setw(20) << pl.name << ": " 
+			cout << ct++ << setw(LIM) << pl.name << ": " 
 				<< setprecision(0) << setw(12) << pl.population
 				<< setprecision(2) << setw(6) << pl.g << endl;
 		}		
-		fin.close();		
+		if (finout.eof())
+			finout.clear();
+		else
+		{
+			cerr << "Error is reading " << file << ".\n";
+			exit(EXIT_FAILURE);
+		}
 	}
-	ofstream fout(file, ios_base::out|ios_base::app|ios_base::binary);
-	if (!fout.is_open())
+	else
 	{
-		cerr << "Can't open " << file << " file for output.\n";
+		cerr << file << " could not be opened -- bye.\n";
 		exit(EXIT_FAILURE);
 	}
-	cout << "Enter planet name (enter a blanc line to quit):\n";
-	cin.get(pl.name, 20);
-	while (pl.name[0] != '\0') 
+	cout << "Enter the record number you wish to change: ";
+	long rec;
+	cin >> rec;
+	eatline();
+	if (rec < 0 || rec >= ct)
 	{
-		eatline();
-		cout << "Enter planetary population: ";
-		cin >> pl.population;
-		cout << "Enter planet's acceleration of gravity: ";
-		cin >> pl.g;
-		eatline();
-		fout.write((char*) &pl, sizeof pl);
-		cout << "Enter planet name (enter a blanc line to quit):\n";
-		cin.get(pl.name, 20);
+		cerr << "Invalid record number -- bye.\n";
+		exit(EXIT_FAILURE);
 	}
-	fout.close();
-	//fin.clear();
-	fin.open(file, ios_base::in|ios_base::binary);
-	if (fin.is_open())
+	streampos place = rec * sizeof(pl); //!!!!!!!!!!!
+	finout.seekg(place);
+	if (finout.fail())
 	{
-		cout << "Here are the new contents of the " << file << " file:\n";
-		while (fin.read((char*) &pl, sizeof pl))
-		{
-			cout << setw(20) << pl.name << ": " 
-				<< setprecision(0) << setw(12) << pl.population
-				<< setprecision(2) << setw(6) << pl.g << endl;
-		}		
-		fin.close();		
+		cerr << "Error on attempted seek.\n";
+		exit(EXIT_FAILURE);
 	}
+	finout.read((char*) &pl, sizeof pl);
+	cout << "Your selection: \n";
+	cout << rec << ": " << setw(LIM) << pl.name << ": " 
+		<< setprecision(0) << setw(12) << pl.population
+		<< setprecision(2) << setw(6) << pl.g << endl;
+	if (finout.eof())
+		finout.clear();
+	cout << "Enter planet name: ";
+	cin.get(pl.name, LIM);
+	eatline();
+	cout << "Enter planetary population: ";
+	cin >> pl.population;
+	cout << "Enter planet's acceleration of gravity: ";
+	cin >> pl.g;
+	finout.seekp(place);
+	finout.write((char*) &pl, sizeof pl) << flush;
+	if (finout.fail())
+	{
+		cerr << "Error on attempted write.\n";
+		exit(EXIT_FAILURE);
+	}
+	ct = 0;
+	finout.seekg(0);
+	cout << "Here are the new contents of the " << file << " file:\n";
+	while (finout.read((char*) &pl, sizeof pl))
+	{
+		cout << ct++ << setw(LIM) << pl.name << ": " 
+			<< setprecision(0) << setw(12) << pl.population
+			<< setprecision(2) << setw(6) << pl.g << endl;
+	}		
+	finout.close();
 	cout << "Done.\n";
 	return 0;
 }
