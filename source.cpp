0f85081618abb6d682c9d9541ddfcfa996062bba
Vladimir P <pustovalovv455@gmail.com>
2022-05-30 01:23:37 +0700

    Prata 17-21

--- source.cpp
+++ source.cpp
@@ -1,97 +1,19 @@
-//#include</home/bobs/Prata/Header.h> 
+#include<sstream>
 #include<iostream>
-#include<fstream>
-#include<iomanip>
-#include<cstdlib>
+#include<string>
 
-const int LIM = 20;
-inline void eatline() { while (std::cin.get() != '\n') continue; }
-struct planet
+int main()
 {
-	char name[LIM];
-	double population;
-	double g;
-};
-const char *file = "pythag";
-
-int main()  
-{
-	using namespace std;
-	planet pl;
-	cout << fixed;
-	fstream finout;
-	finout.open(file, ios_base::in|ios_base::binary|ios_base::out);
-	int ct = 0;
-	if (finout.is_open())
-	{
-		finout.seekg(0);
-		cout << "Here are the current contents of the " << file << " file:\n";
-		while (finout.read((char*) &pl, sizeof pl))
-		{
-			cout << ct++ << setw(LIM) << pl.name << ": " 
-				<< setprecision(0) << setw(12) << pl.population
-				<< setprecision(2) << setw(6) << pl.g << endl;
-		}		
-		if (finout.eof())
-			finout.clear();
-		else
-		{
-			cerr << "Error is reading " << file << ".\n";
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		cerr << file << " could not be opened -- bye.\n";
-		exit(EXIT_FAILURE);
-	}
-	cout << "Enter the record number you wish to change: ";
-	long rec;
-	cin >> rec;
-	eatline();
-	if (rec < 0 || rec >= ct)
-	{
-		cerr << "Invalid record number -- bye.\n";
-		exit(EXIT_FAILURE);
-	}
-	streampos place = rec * sizeof(pl); //!!!!!!!!!!!
-	finout.seekg(place);
-	if (finout.fail())
-	{
-		cerr << "Error on attempted seek.\n";
-		exit(EXIT_FAILURE);
-	}
-	finout.read((char*) &pl, sizeof pl);
-	cout << "Your selection: \n";
-	cout << rec << ": " << setw(LIM) << pl.name << ": " 
-		<< setprecision(0) << setw(12) << pl.population
-		<< setprecision(2) << setw(6) << pl.g << endl;
-	if (finout.eof())
-		finout.clear();
-	cout << "Enter planet name: ";
-	cin.get(pl.name, LIM);
-	eatline();
-	cout << "Enter planetary population: ";
-	cin >> pl.population;
-	cout << "Enter planet's acceleration of gravity: ";
-	cin >> pl.g;
-	finout.seekp(place);
-	finout.write((char*) &pl, sizeof pl) << flush;
-	if (finout.fail())
-	{
-		cerr << "Error on attempted write.\n";
-		exit(EXIT_FAILURE);
-	}
-	ct = 0;
-	finout.seekg(0);
-	cout << "Here are the new contents of the " << file << " file:\n";
-	while (finout.read((char*) &pl, sizeof pl))
-	{
-		cout << ct++ << setw(LIM) << pl.name << ": " 
-			<< setprecision(0) << setw(12) << pl.population
-			<< setprecision(2) << setw(6) << pl.g << endl;
-	}		
-	finout.close();
-	cout << "Done.\n";
-	return 0;
+using namespace std;
+ostringstream outstr;
+string hdisc;
+cout << "What's the name of your hard disc? ";
+getline(cin, hdisc);
+int cap;
+cout << "What's its capasity in GB? ";
+cin >> cap;
+outstr << "The hard disc " << hdisc << " has a capasity of " << cap << " gigabytes.\n";
+string result = outstr.str();
+cout << result;
+return 0;
 }
