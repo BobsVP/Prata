e0f88c892bb267c49b86eeb393f3029c39009482
Vladimir P <pustovalovv455@gmail.com>
2022-05-30 01:29:46 +0700

    Prata 17-22

--- source.cpp
+++ source.cpp
@@ -5,15 +5,10 @@
 int main()
 {
 using namespace std;
-ostringstream outstr;
-string hdisc;
-cout << "What's the name of your hard disc? ";
-getline(cin, hdisc);
-int cap;
-cout << "What's its capasity in GB? ";
-cin >> cap;
-outstr << "The hard disc " << hdisc << " has a capasity of " << cap << " gigabytes.\n";
-string result = outstr.str();
-cout << result;
+string lit = "It was a dark and stormy day, and the full moon glowed brilliantly. ";
+istringstream instr(lit);
+string word;
+while (instr >> word)
+	cout << word << endl;
 return 0;
 }
