36fb3ea0b0c684bd4da54a11ff9c349077671aec
Vladimir <pustovalovv455@gmail.com>
2022-11-12 01:38:25 +0700

    Prata 18.4 lambda0.cpp

--- source.cpp
+++ source.cpp
@@ -1,149 +1,47 @@
 #include<iostream>
-#include<utility>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<ctime>
 
-class Useless
-{
-private:
-	int n;
-	char* pc;
-	static int ct;
-	void ShowObject() const;
-public:
-	Useless();
-	explicit Useless(int k);
-	Useless(int k, char ch);
-	Useless(const Useless& f);
-	Useless(Useless&& f);
-	~Useless();
-	Useless operator+(const Useless& f) const;
-	Useless& operator=(const Useless& f);
-	Useless& operator=(Useless&& f);
-	void ShowData() const;
-};
-
-int Useless::ct = 0;
-
-Useless::Useless()
-{
-	++ct;
-	n = 0;
-	pc = nullptr;
-}
-
-Useless::Useless(int k) : n(k)
-{
-	++ct;
-	pc = new char[n];
-}
-
-Useless::Useless(int k, char ch) : n(k)
-{
-	++ct;
-	pc = new char[n];
-	for (int i = 0; i < n; ++i)
-		pc[i] = ch;
-}
-
-Useless::Useless(const Useless& f) : n(f.n)
-{
-	++ct;
-	pc = new char[n];
-	for (int i = 0; i < n; ++i)
-		pc[i] = f.pc[i];
-}
-
-Useless::Useless(Useless&& f) : n(f.n)
-{
-	++ct;
-	pc = f.pc;
-	f.pc = nullptr;
-	f.n = 0;
-}
-
-Useless::~Useless()
-{
-	delete[] pc;
-}
-
-Useless& Useless::operator=(const Useless& f)
-{
-	std::cout << "copy assignment operator called:\n";
-	if (this == &f)
-		return *this;
-	delete[] pc;
-	n = f.n;
-	pc = new char[n];
-	for (int i = 0; i < n; ++i)
-		pc[i] = f.pc[i];
-	return *this;
-}
-
-Useless& Useless::operator=(Useless&& f)
-{
-	std::cout << "move assignment operator called:\n";
-	if (this == &f)
-		return *this;
-	delete[] pc;
-	n = f.n;
-	pc = f.pc;
-	f.n = 0;
-	f.pc = nullptr;
-	return *this;
-}
-
-Useless Useless::operator+(const Useless& f) const
-{
-	Useless temp = Useless(n + f.n);
-	for (int i = 0; i < n; ++i)
-		temp.pc[i] = pc[i];
-	for (int i = n; i < temp.n; ++i)
-		temp.pc[i] = f.pc[i - n];
-	return temp;
-}
-
-
-void Useless::ShowObject() const
-{
-	std::cout << "Number of elements: " << n;
-	std::cout << " Data adress: " << (void*)pc << std::endl;
-}
-
-void Useless::ShowData() const
-{
-	if (n == 0)
-		std::cout << "(object empty)";
-	else
-		for (int i = 0; i < n; ++i)
-			std::cout << pc[i];
-	std::cout << std::endl;
-}
+const long Size1 = 39L;
+const long Size2 = 100 * Size1;
+const long Size3 = 100 * Size2;
+bool f3(int x) { return x % 3 == 0; }
+bool f13(int x) { return x % 13 == 0; }
 
 int main()
 {
 	using std::cout;
+	std::vector<int> numbers(Size1);
+	std::srand(std::time(0));
+	std::generate(numbers.begin(), numbers.end(), std::rand);
+	cout << "Sample size = " << Size1 << '\n';
+	int count3 = std::count_if(numbers.begin(), numbers.end(), f3);
+	cout << "Counts of numbers divisible by 3: " << count3 << '\n';
+	int count13 = std::count_if(numbers.begin(), numbers.end(), f13);
+	cout << "Counts of numbers divisible by 13: " << count13 << '\n';
+	numbers.resize(Size2);
+	std::generate(numbers.begin(), numbers.end(), std::rand);
+	cout << "Sample size = " << Size2 << '\n';
+	class f_mod
 	{
-		Useless one(10, 'x');
-		Useless two = one + one;
-		cout << "object one: ";
-		one.ShowData();
-		cout << "object two: ";
-		two.ShowData();
-		Useless three, four;
-		cout << "three = one\n";
-		three = one;
-		cout << "new object three = ";
-		three.ShowData();
-		cout << "and object one = ";
-		one.ShowData();
-		cout << "four = one + two\n";
-		four = one + two;
-		cout << "new object four = ";
-		four.ShowData();
-		cout << "four = move(one)\n";
-		four = std::move(one);
-		cout << "new object four = ";
-		four.ShowData();
-		cout << "and object one = ";
-		one.ShowData();
-	}
+	private:
+		int dv;
+	public:
+		f_mod(int d = 1) : dv(d) {}
+		bool operator()(int x) { return x % dv == 0; }
+	};
+	count3 = std::count_if(numbers.begin(), numbers.end(), f_mod(3));
+	cout << "Counts of numbers divisible by 3: " << count3 << '\n';
+	count13 = std::count_if(numbers.begin(), numbers.end(), f_mod(13));
+	cout << "Counts of numbers divisible by 13: " << count13 << '\n';
+	numbers.resize(Size3);
+	std::generate(numbers.begin(), numbers.end(), std::rand);
+	cout << "Sample size = " << Size3 << '\n';
+	count3 = std::count_if(numbers.begin(), numbers.end(), [](int x) {return x % 3 == 0; });
+	cout << "Counts of numbers divisible by 3: " << count3 << '\n';
+	count13 = std::count_if(numbers.begin(), numbers.end(), [](int x) {return x % 13 == 0; });
+	cout << "Counts of numbers divisible by 13: " << count13 << '\n';
+	return 0;
 }
