88c0daec57b1cdaa3f9a1541b04d203b4c5a6f5d
BobsVP <pustovalovv455@gmail.com>
2022-11-13 02:17:49 +0700

    Prata 18.5 lambda1.cpp

--- source.cpp
+++ source.cpp
@@ -4,44 +4,23 @@
 #include<cmath>
 #include<ctime>
 
-const long Size1 = 39L;
-const long Size2 = 100 * Size1;
-const long Size3 = 100 * Size2;
-bool f3(int x) { return x % 3 == 0; }
-bool f13(int x) { return x % 13 == 0; }
+const long Size = 390000L;
 
 int main()
 {
 	using std::cout;
-	std::vector<int> numbers(Size1);
+	std::vector<int> numbers(Size);
 	std::srand(std::time(0));
 	std::generate(numbers.begin(), numbers.end(), std::rand);
-	cout << "Sample size = " << Size1 << '\n';
-	int count3 = std::count_if(numbers.begin(), numbers.end(), f3);
+	cout << "Sample size = " << Size << '\n';
+	int count3 = std::count_if(numbers.begin(), numbers.end(), [](int x) {return x % 3 == 0; });
 	cout << "Counts of numbers divisible by 3: " << count3 << '\n';
-	int count13 = std::count_if(numbers.begin(), numbers.end(), f13);
+	int count13 = 0;
+	std::for_each(numbers.begin(), numbers.end(), [&count13](int x) {count13 += x % 13 == 0; });
 	cout << "Counts of numbers divisible by 13: " << count13 << '\n';
-	numbers.resize(Size2);
-	std::generate(numbers.begin(), numbers.end(), std::rand);
-	cout << "Sample size = " << Size2 << '\n';
-	class f_mod
-	{
-	private:
-		int dv;
-	public:
-		f_mod(int d = 1) : dv(d) {}
-		bool operator()(int x) { return x % dv == 0; }
-	};
-	count3 = std::count_if(numbers.begin(), numbers.end(), f_mod(3));
+	count3 = count13 = 0;
+	std::for_each(numbers.begin(), numbers.end(), [&](int x) {count3 += x % 3 == 0; count13 += x % 13 == 0; });
 	cout << "Counts of numbers divisible by 3: " << count3 << '\n';
-	count13 = std::count_if(numbers.begin(), numbers.end(), f_mod(13));
-	cout << "Counts of numbers divisible by 13: " << count13 << '\n';
-	numbers.resize(Size3);
-	std::generate(numbers.begin(), numbers.end(), std::rand);
-	cout << "Sample size = " << Size3 << '\n';
-	count3 = std::count_if(numbers.begin(), numbers.end(), [](int x) {return x % 3 == 0; });
-	cout << "Counts of numbers divisible by 3: " << count3 << '\n';
-	count13 = std::count_if(numbers.begin(), numbers.end(), [](int x) {return x % 13 == 0; });
 	cout << "Counts of numbers divisible by 13: " << count13 << '\n';
 	return 0;
 }
