5a348fd4712e945b176bde17a05804c302df3d74
Vladimir <pustovalovv455@gmail.com>
2022-11-18 01:39:57 +0700

    Prata 18.10 variadic2.cpp

--- source.cpp
+++ source.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<string>
 
-void show_list3() {}
+void show_list() {}
+
+template<typename T>
+void show_list(T value)
+{
+	std::cout << value << '\n';
+}
 
 template<typename T, typename... Args>
-void show_list3(T value, Args... args) 
+void show_list(T value, Args... args)
 {
 	std::cout << value << ", ";
-	show_list3(args...);
+	show_list(args...);
 }
 
 int main()
@@ -15,7 +21,7 @@ int main()
 	int n = 14;
 	double x = 2.71828;
 	std::string mr = "Mr. String objects!";
-	show_list3(n, x);
-	show_list3(x * x, '!', 7, mr);
+	show_list(n, x);
+	show_list(x * x, '!', 7, mr);
 	return 0;
 }
